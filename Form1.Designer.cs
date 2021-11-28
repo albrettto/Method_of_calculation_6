3ad1770d0c30017e9d781af1ce1556c58e2bb3a0
khusamov01@icloud.com <a.husamov@yandex.ru>
2021-11-28 20:18:32 +0500

    Изменил типа

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Method_of_calculation_6
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Result_btn = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Honeydew;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(679, 547);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Result_btn
+            // 
+            this.Result_btn.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.Result_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Result_btn.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.Result_btn.Location = new System.Drawing.Point(288, 566);
+            this.Result_btn.Name = "Result_btn";
+            this.Result_btn.Size = new System.Drawing.Size(132, 35);
+            this.Result_btn.TabIndex = 1;
+            this.Result_btn.Text = "Рассчитать";
+            this.Result_btn.UseVisualStyleBackColor = false;
+            this.Result_btn.Click += new System.EventHandler(this.Result_btn_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "n";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "f(0)";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 175;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "f(pi/4)";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 175;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "f(pi/2)";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 175;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(706, 613);
+            this.Controls.Add(this.Result_btn);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа 6";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button Result_btn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Method_of_calculation_6
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        static int end = 20;
+        static int y0 = 1;
+        static double a = 0;
+        static double b = Math.PI / 2;
+        static double[] start_section = new double[end];
+        static double[] mid_section = new double[end];
+        static double[] end_section = new double[end];
+
+        // 
+        static double Func(double x, double y)
+        {
+            return y * Math.Sin(x);
+        }
+
+        // 
+        static double Ex_sol(double x)
+        {
+            return Math.Exp(1 - Math.Cos(x));
+        }
+
+        static double[] Eulers_method(int n)
+        {
+            double[] yj = new double[n + 1];
+            yj[0] = y0;
+            double h = (a + b) / n;
+
+            for (int i = 1; i < n; ++i)
+            {
+                double xj = a + i * h;
+                yj[i] = yj[i - 1] + h * Func(xj, yj[i - 1]);
+            }
+            return yj;
+        }
+
+        private void Result_btn_Click(object sender, EventArgs e)
+        {
+            int n = 1;
+            double mid = (a + b) / 2;
+
+            for (int i = 0; i < end; ++i)
+            {
+                n *= 2;
+                double[] yj = Eulers_method(n);
+
+                start_section[i] = Ex_sol(0) - yj[0];
+                mid_section[i] = Ex_sol(mid) - yj[(n-1) / 2];
+                end_section[i] = Ex_sol(b) - yj[n - 1];
+                dataGridView.Rows.Add(n, start_section[i], mid_section[i], end_section[i]);
+            }
+        }
+    }
+}
